e32879cfb7f9a6c6fe0925e325d2832536b552ad
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-02-28 11:46:38 -0800

    Add app integration test for train to rank
    * Add test that runs cawlr train on a small example of processed data
    and runs cawlr rank on the data
    * Reformatting
    * add setup method for easily creating multiple instances of TestFiles

--- tests/train.rs
+++ tests/train.rs
@@ -1,12 +1,15 @@
-use std::ffi::OsStr;
-use std::fs::File;
+use std::{ffi::OsStr, fs::File};
 
 use anyhow::Result;
 use assert_cmd::Command;
-use assert_fs::fixture::ChildPath;
-use assert_fs::{fixture::PathChild, TempDir};
-use polars::io::prelude::ParquetWriter;
-use polars::prelude::{DataFrame, NamedFrom, Series};
+use assert_fs::{
+    fixture::{ChildPath, PathChild},
+    NamedTempFile, TempDir,
+};
+use polars::{
+    io::prelude::ParquetWriter,
+    prelude::{DataFrame, NamedFrom, Series},
+};
 
 struct TestFiles {
     // Keep around TempDir to extend lifetime
@@ -30,6 +33,22 @@ impl TestFiles {
 
     fn output(&self) -> &OsStr {
         self.output.as_os_str()
+    }
+    fn setup(input_filename: &str, output_filename: &str) -> Result<Self> {
+        let temp_dir = TempDir::new()?;
+        let input_file_path = temp_dir.child(input_filename);
+        let input_file = File::create(&input_file_path)?;
+        let df = polars::df!("event_mean" => &[0.1, 0.2, 0.3, 0.4, 0.5],
+                                    "kmer" => &["AAAAAA", "AAAAAA", "AAAAAA", "AAAAAA", "AAAAAA"])?;
+        ParquetWriter::new(input_file).finish(&df)?;
+
+        let output_file_path = temp_dir.child(output_filename);
+        let test_files = TestFiles::new(temp_dir, input_file_path, output_file_path);
+        Ok(test_files)
+    }
+
+    fn sim_processed_data(n_kmers: usize, samples_per_kmer: usize) -> DataFrame {
+        unimplemented!()
     }
 }
 
@@ -56,5 +75,43 @@ fn test_train() -> Result<()> {
         .arg("-o")
         .arg(test_files.output());
     cmd.assert().try_success()?;
+    Ok(())
+}
+
+#[test]
+fn test_train_to_rank() -> Result<()> {
+    let pos_ctrl_files = TestFiles::setup("pos_ctrl.train", "pos_ctrl.models")?;
+    let neg_ctrl_files = TestFiles::setup("neg_ctrl.train", "neg_ctrl.models")?;
+
+    let mut pos_cmd = Command::cargo_bin("cawlr")?;
+    pos_cmd
+        .arg("train")
+        .arg("-i")
+        .arg(pos_ctrl_files.input())
+        .arg("-o")
+        .arg(pos_ctrl_files.output());
+    pos_cmd.assert().try_success()?;
+
+    let mut neg_cmd = Command::cargo_bin("cawlr")?;
+    neg_cmd
+        .arg("train")
+        .arg("-i")
+        .arg(neg_ctrl_files.input())
+        .arg("-o")
+        .arg(neg_ctrl_files.output());
+    neg_cmd.assert().try_success()?;
+
+    let result = NamedTempFile::new("result")?;
+    let mut rank_cmd = Command::cargo_bin("cawlr")?;
+    rank_cmd
+        .arg("rank")
+        .arg("--pos-ctrl")
+        .arg(pos_ctrl_files.output())
+        .arg("--neg-ctrl")
+        .arg(neg_ctrl_files.output())
+        .arg("-o")
+        .arg(result.path());
+    rank_cmd.assert().try_success()?;
+
     Ok(())
 }
