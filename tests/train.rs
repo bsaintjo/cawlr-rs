23e86ac3e6d31e1732ce51e16b0cdea2065b5642
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-02-28 11:51:09 -0800

    Use TestFiles::setup

--- tests/train.rs
+++ tests/train.rs
@@ -52,22 +52,9 @@ impl TestFiles {
     }
 }
 
-fn setup() -> Result<TestFiles> {
-    let temp_dir = TempDir::new()?;
-    let input_file_path = temp_dir.child("test.parquet");
-    let input_file = File::create(&input_file_path)?;
-    let df = polars::df!("event_mean" => &[0.1, 0.2, 0.3, 0.4, 0.5],
-                                  "kmer" => &["AAAAAA", "AAAAAA", "AAAAAA", "AAAAAA", "AAAAAA"])?;
-    ParquetWriter::new(input_file).finish(&df)?;
-
-    let output_file_path = temp_dir.child("results");
-    let test_files = TestFiles::new(temp_dir, input_file_path, output_file_path);
-    Ok(test_files)
-}
-
 #[test]
 fn test_train() -> Result<()> {
-    let test_files = setup()?;
+    let test_files = TestFiles::setup("train", "model")?;
     let mut cmd = Command::cargo_bin("cawlr")?;
     cmd.arg("train")
         .arg("-i")
