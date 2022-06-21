16d79c13dc7a14e99450909f560b8d9a18453864
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-06-21 16:39:59 -0700

    Make pipeline testing more modular

--- tests/pipeline.rs
+++ tests/pipeline.rs
@@ -1,10 +1,18 @@
 use anyhow::Result;
 use cawlr::CollapseOptions;
 
+fn test_ctrl(file_path: &str) -> Result<()> {
+    let collapse_opts = CollapseOptions::try_new(file_path, "/dev/null", 4096)?;
+    collapse_opts.run()
+}
+
 #[test_log::test]
-fn test_pos_ctrl() -> Result<()> {
-    let pos_eventalign = "extra/pos_eventalign.txt";
-    let collapse_opts = CollapseOptions::try_new(pos_eventalign, "/dev/null", 4096)?;
-    collapse_opts.run()?;
+fn pipeline() -> Result<()> {
+    let pos_ctrl = "extra/pos_control.eventalign.txt";
+    let neg_ctrl = "extra/neg_control.eventalign.txt";
+
+    test_ctrl(pos_ctrl)?;
+    test_ctrl(neg_ctrl)?;
+
     Ok(())
 }
