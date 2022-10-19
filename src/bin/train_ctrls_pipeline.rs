8f3faa057eaedab8f38606e96ce9a0a140761f9b
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-10-19 14:14:30 -0700

    feat: pipeline from training controls

---
+++ src/bin/train_ctrls_pipeline.rs
@@ -0,0 +1,59 @@
+use std::{fs, path::PathBuf, process::Command};
+
+use cawlr::utils;
+use clap::Parser;
+use eyre::Result;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    genome: PathBuf,
+
+    #[clap(short, long)]
+    pos_fast5s: PathBuf,
+
+    #[clap(short, long)]
+    pos_reads: PathBuf,
+
+    #[clap(short, long)]
+    neg_fast5s: PathBuf,
+
+    #[clap(short, long)]
+    neg_reads: PathBuf,
+
+    #[clap(short, long)]
+    output_dir: PathBuf,
+
+    #[clap(long)]
+    nanopolish_path: Option<PathBuf>,
+}
+
+fn aln_reads(
+    minimap2: PathBuf,
+    genome: PathBuf,
+    reads: PathBuf,
+    output: PathBuf,
+) -> eyre::Result<()> {
+    Command::new(minimap2)
+        .arg("-ax")
+        .arg("map-ont")
+        .arg("--sam-hit-only")
+        .arg("--secondary=no")
+        .args(&["-t", "4"])
+        .arg(genome)
+        .arg(reads)
+        .arg("-o")
+        .arg(output);
+    Ok(())
+}
+
+fn main() -> eyre::Result<()> {
+    let args = Args::parse();
+    let nanopolish = utils::find_binary("nanopolish", &args.nanopolish_path)?;
+    let minimap2 = utils::find_binary("minimap2", &args.nanopolish_path)?;
+    let samtools = utils::find_binary("samtools", &args.nanopolish_path)?;
+
+    fs::create_dir_all(&args.output_dir)?;
+
+    Ok(())
+}
