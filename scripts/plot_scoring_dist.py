0dfe3af84162f34efa792a235882eaea25db750d
Brandon Saint-John <bsaintjo@ucsc.edu>
2023-01-09 22:27:47 -0800

    update scoring dist plotter

--- scripts/plot_scoring_dist.py
+++ scripts/plot_scoring_dist.py
@@ -1,32 +1,52 @@
 #!/usr/bin/env python3
 
 import argparse
-from typing import List
-import pyarrow.feather as feather
 import matplotlib.pyplot as plt
+import pickle
+from dataclasses import dataclass
+import itertools
 
-def table_to_gmm_scores(table) -> List[float]:
-    scores = []
-    for chunk in table:
-        for read in chunk:
-            for score in read["scores"]:
-                signal_score = score["signal_score"].as_py()
-                kmer = score["kmer"]
-                if signal_score is not None:
-                    scores.append(signal_score)
-    return scores
+
+@dataclass
+class ScoreDist:
+    filename: str
+    dist: list[float]
+    color: str
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", nargs="+", help="Score files to plot distribution")
+    parser.add_argument(
+        "-i", "--input", nargs="+", help="Score files to plot distribution"
+    )
     parser.add_argument("-o", "--output", required=True, help="Output filename")
 
     args = parser.parse_args()
 
-    score_file = args.input[0]
-    table = feather.read_table(score_file)
-    print(table)
+    all_sdists = []
+    colors = itertools.cycle(["red", "blue", "green", "orange", "yellow"])
+    for ms_filepath, color in zip(args.input, colors):
+        with open(ms_filepath, "rb") as ms_file:
+            data = pickle.load(ms_file)
+            dist = data["bins"]
+            sdist = ScoreDist(ms_filepath, dist, color)
+            all_sdists.append(sdist)
+    for sdist in all_sdists:
+        n_bins = len(sdist.dist)
+        dist_sum = sum(sdist.dist)
+        bar_heights = [x / dist_sum for x in sdist.dist]
+        plt.bar(
+            x=[a / n_bins for a in range(0, n_bins)],
+            height=bar_heights,
+            width=1 / n_bins,
+            color=sdist.color,
+            alpha=0.3,
+            label=sdist.filename,
+        )
+    plt.legend()
+    plt.xlabel("Score")
+    plt.ylabel("Density")
+    plt.savefig(args.output)
 
 
 if __name__ == "__main__":
